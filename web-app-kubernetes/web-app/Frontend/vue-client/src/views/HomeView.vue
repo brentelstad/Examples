46a543c295611be39a7cd7276b2118d454f81771
Belstad <brentelstad@outlook.com>
2025-11-15 08:55:04 -0600

    remove boiler plate vue code

--- web-app-kubernetes/web-app/Frontend/vue-client/src/views/HomeView.vue
+++ web-app-kubernetes/web-app/Frontend/vue-client/src/views/HomeView.vue
@@ -5,14 +5,11 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
 </script>
+
+<style scoped>
+
+</style>
